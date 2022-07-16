532899b3c775d64b55a1b7bdc9f463712dd27e7f
whiteman007 <108463448+whiteman007@users.noreply.github.com>
2022-07-16 21:21:11 +0300

    Add files via upload

---
+++ test/mempool_tests.cpp
@@ -0,0 +1,107 @@
+// Copyright (c) 2011-2014 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "main.h"
+#include "txmempool.h"
+#include "util.h"
+
+#include "test/test_bitcoin.h"
+
+#include <boost/test/unit_test.hpp>
+#include <list>
+
+BOOST_FIXTURE_TEST_SUITE(mempool_tests, TestingSetup)
+
+BOOST_AUTO_TEST_CASE(MempoolRemoveTest)
+{
+    // Test CTxMemPool::remove functionality
+
+    // Parent transaction with three children,
+    // and three grand-children:
+    CMutableTransaction txParent;
+    txParent.vin.resize(1);
+    txParent.vin[0].scriptSig = CScript() << OP_11;
+    txParent.resizeOut(3);
+    for (int i = 0; i < 3; i++)
+    {
+        txParent.getOut(i).scriptPubKey = CScript() << OP_11 << OP_EQUAL;
+        txParent.getOut(i).nValue = 33000LL;
+    }
+    CMutableTransaction txChild[3];
+    for (int i = 0; i < 3; i++)
+    {
+        txChild[i].vin.resize(1);
+        txChild[i].vin[0].scriptSig = CScript() << OP_11;
+        txChild[i].vin[0].prevout.hash = txParent.GetHash();
+        txChild[i].vin[0].prevout.n = i;
+        txChild[i].resizeOut(1);
+        txChild[i].getOut(0).scriptPubKey = CScript() << OP_11 << OP_EQUAL;
+        txChild[i].getOut(0).nValue = 11000LL;
+    }
+    CMutableTransaction txGrandChild[3];
+    for (int i = 0; i < 3; i++)
+    {
+        txGrandChild[i].vin.resize(1);
+        txGrandChild[i].vin[0].scriptSig = CScript() << OP_11;
+        txGrandChild[i].vin[0].prevout.hash = txChild[i].GetHash();
+        txGrandChild[i].vin[0].prevout.n = 0;
+        txGrandChild[i].resizeOut(1);
+        txGrandChild[i].getOut(0).scriptPubKey = CScript() << OP_11 << OP_EQUAL;
+        txGrandChild[i].getOut(0).nValue = 11000LL;
+    }
+
+
+    CTxMemPool testPool(CFeeRate(0));
+    std::list<CTransaction>   removedTxs;
+    std::list<CScCertificate> removedCerts;
+
+    // Nothing in pool, remove should do nothing:
+    testPool.remove(txParent, removedTxs, removedCerts, true);
+    BOOST_CHECK_EQUAL(removedTxs.size(), 0);
+    BOOST_CHECK_EQUAL(removedCerts.size(), 0);
+
+    // Just the parent:
+    testPool.addUnchecked(txParent.GetHash(), CTxMemPoolEntry(txParent, 0, 0, 0.0, 1));
+    testPool.remove(txParent, removedTxs, removedCerts, true);
+    BOOST_CHECK_EQUAL(removedTxs.size(), 1);
+    BOOST_CHECK_EQUAL(removedCerts.size(), 0);
+    removedTxs.clear();
+    
+    // Parent, children, grandchildren:
+    testPool.addUnchecked(txParent.GetHash(), CTxMemPoolEntry(txParent, 0, 0, 0.0, 1));
+    for (int i = 0; i < 3; i++)
+    {
+        testPool.addUnchecked(txChild[i].GetHash(), CTxMemPoolEntry(txChild[i], 0, 0, 0.0, 1));
+        testPool.addUnchecked(txGrandChild[i].GetHash(), CTxMemPoolEntry(txGrandChild[i], 0, 0, 0.0, 1));
+    }
+    // Remove Child[0], GrandChild[0] should be removed:
+    testPool.remove(txChild[0], removedTxs, removedCerts, true);
+    BOOST_CHECK_EQUAL(removedTxs.size(), 2);
+    removedTxs.clear();
+    // ... make sure grandchild and child are gone:
+    testPool.remove(txGrandChild[0], removedTxs, removedCerts, true);
+    BOOST_CHECK_EQUAL(removedTxs.size(), 0);
+    testPool.remove(txChild[0], removedTxs, removedCerts, true);
+    BOOST_CHECK_EQUAL(removedTxs.size(), 0);
+    // Remove parent, all children/grandchildren should go:
+    testPool.remove(txParent, removedTxs, removedCerts, true);
+    BOOST_CHECK_EQUAL(removedTxs.size(), 5);
+    BOOST_CHECK_EQUAL(testPool.size(), 0);
+    removedTxs.clear();
+
+    // Add children and grandchildren, but NOT the parent (simulate the parent being in a block)
+    for (int i = 0; i < 3; i++)
+    {
+        testPool.addUnchecked(txChild[i].GetHash(), CTxMemPoolEntry(txChild[i], 0, 0, 0.0, 1));
+        testPool.addUnchecked(txGrandChild[i].GetHash(), CTxMemPoolEntry(txGrandChild[i], 0, 0, 0.0, 1));
+    }
+    // Now remove the parent, as might happen if a block-re-org occurs but the parent cannot be
+    // put into the mempool (maybe because it is non-standard):
+    testPool.remove(txParent, removedTxs, removedCerts, true);
+    BOOST_CHECK_EQUAL(removedTxs.size(), 6);
+    BOOST_CHECK_EQUAL(testPool.size(), 0);
+    removedTxs.clear();
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ test/test_bitcoin.cpp
@@ -0,0 +1,136 @@
+// Copyright (c) 2011-2013 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#define BOOST_TEST_MODULE Bitcoin Test Suite
+
+#include "test_bitcoin.h"
+
+#include "crypto/common.h"
+
+#include "key.h"
+#include "main.h"
+#include "random.h"
+#include "txdb.h"
+#include "ui_interface.h"
+#include "util.h"
+#ifdef ENABLE_WALLET
+#include "wallet/db.h"
+#include "wallet/wallet.h"
+#endif
+
+#include <boost/filesystem.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/thread.hpp>
+
+#include "librustzcash.h"
+
+CClientUIInterface uiInterface; // Declared but not defined in ui_interface.h
+CWallet* pwalletMain;
+ZCJoinSplit *pzcashParams;
+
+extern bool fPrintToConsole;
+extern void noui_connect();
+
+JoinSplitTestingSetup::JoinSplitTestingSetup()
+{
+    boost::filesystem::path pk_path = ZC_GetParamsDir() / "sprout-proving.key";
+    boost::filesystem::path vk_path = ZC_GetParamsDir() / "sprout-verifying.key";
+    pzcashParams = ZCJoinSplit::Prepared(vk_path.string(), pk_path.string());
+    boost::filesystem::path sapling_spend = ZC_GetParamsDir() / "sapling-spend.params";
+    boost::filesystem::path sapling_output = ZC_GetParamsDir() / "sapling-output.params";
+    boost::filesystem::path sprout_groth16 = ZC_GetParamsDir() / "sprout-groth16.params";
+
+    std::string sapling_spend_str = sapling_spend.string();
+    std::string sapling_output_str = sapling_output.string();
+    std::string sprout_groth16_str = sprout_groth16.string();
+
+    librustzcash_init_zksnark_params(
+        sapling_spend_str.c_str(),
+        "8270785a1a0d0bc77196f000ee6d221c9c9894f55307bd9357c3f0105d31ca63991ab91324160d8f53e2bbd3c2633a6eb8bdf5205d822e7f3f73edac51b2b70c",
+        sapling_output_str.c_str(),
+        "657e3d38dbb5cb5e7dd2970e8b03d69b4787dd907285b5a7f0790dcc8072f60bf593b32cc2d1c030e00ff5ae64bf84c5c3beb84ddc841d48264b4a171744d028",
+        sprout_groth16_str.c_str(),
+        "e9b238411bd6c0ec4791e9d04245ec350c9c5744f5610dfcce4365d5ca49dfefd5054e371842b3f88fa1b9d7e8e075249b3ebabd167fa8b0f3161292d36c180a"
+    );
+}
+
+JoinSplitTestingSetup::~JoinSplitTestingSetup()
+{
+    delete pzcashParams;
+}
+
+BasicTestingSetup::BasicTestingSetup()
+{
+    assert(init_and_check_sodium() != -1);
+    ECC_Start();
+    SetupEnvironment();
+    fPrintToDebugLog = false; // don't want to write to debug.log file
+    fCheckBlockIndex = true;
+    SelectParams(CBaseChainParams::MAIN);
+}
+BasicTestingSetup::~BasicTestingSetup()
+{
+    ECC_Stop();
+}
+
+TestingSetup::TestingSetup()
+{
+#ifdef ENABLE_WALLET
+        bitdb.MakeMock();
+#endif
+        ClearDatadirCache();
+        pathTemp = GetTempPath() / strprintf("test_bitcoin_%lu_%i", (unsigned long)GetTime(), (int)(GetRand(100000)));
+        boost::filesystem::create_directories(pathTemp);
+        mapArgs["-datadir"] = pathTemp.string();
+        pblocktree = new CBlockTreeDB(1 << 20, true);
+        pcoinsdbview = new CCoinsViewDB(1 << 23, true);
+        pcoinsTip = new CCoinsViewCache(pcoinsdbview);
+        InitBlockIndex();
+#ifdef ENABLE_WALLET
+        bool fFirstRun;
+        pwalletMain = new CWallet("wallet.dat");
+        pwalletMain->LoadWallet(fFirstRun);
+        RegisterValidationInterface(pwalletMain);
+#endif
+        nScriptCheckThreads = 3;
+        for (int i=0; i < nScriptCheckThreads-1; i++)
+            threadGroup.create_thread(&ThreadScriptCheck);
+        RegisterNodeSignals(GetNodeSignals());
+}
+
+TestingSetup::~TestingSetup()
+{
+        UnregisterNodeSignals(GetNodeSignals());
+        threadGroup.interrupt_all();
+        threadGroup.join_all();
+#ifdef ENABLE_WALLET
+        UnregisterValidationInterface(pwalletMain);
+        delete pwalletMain;
+        pwalletMain = NULL;
+#endif
+        UnloadBlockIndex();
+        delete pcoinsTip;
+        delete pcoinsdbview;
+        delete pblocktree;
+#ifdef ENABLE_WALLET
+        bitdb.Flush(true);
+        bitdb.Reset();
+#endif
+        boost::filesystem::remove_all(pathTemp);
+}
+
+void Shutdown(void* parg)
+{
+  exit(0);
+}
+
+void StartShutdown()
+{
+  exit(0);
+}
+
+bool ShutdownRequested()
+{
+  return false;
+}
